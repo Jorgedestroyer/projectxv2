c58555c52504557a4056b4aa0fce555ef9f559bd
Jorgedestroyer <jorgetorresfe79@gmail.com>
2025-10-03 13:43:16 -0300

    Add requirements.txt and update ALLOWED_HOSTS

--- projectx/adoptapet_project/adoptapet/settings.py
+++ projectx/adoptapet_project/adoptapet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pk*imc7=(0gnlpfk1+mu#zphqiwy_%a+1%pf2@tb=!v6wyak83
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
